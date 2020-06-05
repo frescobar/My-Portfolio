8ed9ce10caefacecc2dd7b1f1793e9388779bcd2
frescobar <frescobar87@gmail.com>
2020-06-04 23:13:51 -0500

    Add Header component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Header from "././components/Header"
+import Presentation from "././components/Presentation"
 
 function App() {
   return (
-    <div >
-    </div>
+    <>
+    <Header />
+    <Presentation />
+    
+    </>
   );
 }
 
